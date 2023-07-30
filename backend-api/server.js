fcbf2518adbeea849b8648a099e1556cd34d07b1
vannykin <e0969161@u.nus.edu>
2023-07-30 16:55:51 +0800

    edit server.js

--- backend-api/server.js
+++ backend-api/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // TO SAFEKEEP DB USERNAMES AND PASSWORDS
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
@@ -11,17 +11,23 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-}).then(console.log("MongoDB connected"));
+mongoose
+  .connect(process.env.URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(console.log("MongoDB connected"));
 
-const Course = require('./models/Course');
+const Course = require("./models/Course");
 
 // TO RETURN ALL COURSES IN DATABASE AS AN ARRAY
-app.get('/courses/all', async (req, res) => {
-	const courses = await Course.find(); 
-	res.json(courses);
+app.get("/courses/all", async (req, res) => {
+  const courses = await Course.find();
+  res.json(courses);
+});
+
+app.get("/", async (req, res) => {
+  res.json({ status: "api is working" });
 });
 
 app.listen(3001, () => console.log("Server started on port 3001"));

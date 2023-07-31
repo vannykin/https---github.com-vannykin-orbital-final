7f718ef181704c0169765c9b2f563ba984a3c829
vannykin <e0969161@u.nus.edu>
2023-07-31 16:47:14 +0800

    adding options

--- backend-api/server.js
+++ backend-api/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors(
   {
     origin: ["https://orbital-muddlekillers.vercel.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "OPTIONS"],
     credentials: true
   }
 ));
@@ -27,7 +27,7 @@ const Course = require("./models/Course");
 
 // TO RETURN ALL COURSES IN DATABASE AS AN ARRAY
 app.get("/courses/all", async (req, res) => {
-  // const courses = await Course.find({ code: 'CS2030S' });
+  // const courses = await Course.find();
   const courses = [ { code: 'CS2030S', _id: ObjectId(96154488), name: "testing1", term: "1" }];
   res.json(courses);
 });

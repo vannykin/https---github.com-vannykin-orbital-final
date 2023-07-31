a6652a3ac69085edc7f61d00193a1f33a5fbe475
vannykin <e0969161@u.nus.edu>
2023-07-31 15:58:32 +0800

    changing app.use(cors(...))

--- backend-api/server.js
+++ backend-api/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://orbital-final.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 app.use(express.json());
 
